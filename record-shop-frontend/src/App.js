819364ecf61118b1a3a807413762671d45bb42b1
ciamac <hgzguzg>
2020-03-28 11:39:50 +0100

    Verified by Cia v3.0.1

--- record-shop-frontend/src/App.js
+++ record-shop-frontend/src/App.js
@@ -9,13 +9,32 @@ import UserEditor   from './components/users/Editor';
 import UserList     from './components/users/List';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import AlbumIcon from '@material-ui/icons/Album';
 import PersonIcon from '@material-ui/icons/Person';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import clsx from 'clsx';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const drawerWidth = 240;
+
 
 
 const useStyles = makeStyles(theme => ({
@@ -23,7 +42,8 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
   },
   navBack:{
-    backgroundColor:"#696969"
+    backgroundColor:"#696969",
+    color:"#D3D3D3"
   },
   navjustify:{
     color:"	#D3D3D3" ,
@@ -44,33 +64,153 @@ const useStyles = makeStyles(theme => ({
   orders:{
     color:"red",
     filter: "drop-shadow(0  0.3rem black)",
-  }
-  
- 
-  
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+
+  dis: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+
 }));
 
 function App() {
+  
   const classes = useStyles()
-  return (
-  <div>
-  <AppBar position="static">
-<Toolbar className={classes.navBack}>
-<Table>
-  <TableRow>
-    <TableCell align="center" >
-    <Link to="/admin/records/" className={classes.navjustify}> <AlbumIcon className={classes.records}/>  Records &nbsp;</Link>
-    <Link to="/admin/users/" className={classes.navjustify}><PersonIcon className={classes.users} /> Users &nbsp; </Link>
-    <Link to="/admin/orders/" className={classes.navjustify}><ShoppingCartIcon className={classes.orders} /> Orders &nbsp;</Link>
-    </TableCell>
-  </TableRow>
-</Table>
 
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-</Toolbar>
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return(
+  <div className={classes.dis}>
+  <CssBaseline />
+  <AppBar
+    position="fixed"
+    className={clsx(classes.appBar, {
+      [classes.appBarShift]: open,
+    })}
+  >
+    <Toolbar
+    className={classes.navBack}
+    >
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerOpen}
+        edge="start"
+        className={clsx(classes.menuButton, open && classes.hide)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h4" noWrap
+      className={classes.navjustify}
+      >
+        Record Shop
+      </Typography>
+    </Toolbar>
   </AppBar>
+  <Drawer
+    className={classes.drawer}
+    variant="persistent"
+    anchor="left"
+    open={open}
+    classes={{
+      paper: classes.drawerPaper,
+    }}
+  >
+  
+      <IconButton onClick={handleDrawerClose}>
+        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+      </IconButton>
+    <div className={classes.drawerHeader}>
+    </div>
+    <Divider />
 
+    <List>
+        <Link to="/admin/records/" className={classes.navjustify}> <AlbumIcon className={classes.records}/>  Records &nbsp;</Link>
+    </List>
+
+
+    <List>
+        <Link to="/admin/users/" className={classes.navjustify}><PersonIcon className={classes.users} /> Users &nbsp; </Link>
+    </List>
+
+    <List>
+       <Link to="/admin/orders/" className={classes.navjustify}><ShoppingCartIcon className={classes.orders} /> Orders &nbsp;</Link>
+    </List>
+
+    <Divider />
+  </Drawer>
+  <main
+    className={clsx(classes.content, {
+      [classes.contentShift]: open,
+    })}
+  >
+    <div className={classes.drawerHeader} />
+   
     <Switch>
      <Route path="/admin/orders/:id" component={OrderEditor}/>
       <Route path="/admin/orders/"    component={OrderList}/>
@@ -79,8 +219,11 @@ function App() {
       <Route path="/admin/users/:id"   component={UserEditor}/>
       <Route path="/admin/users/"      component={UserList}/>
     </Switch>
+  </main>
+</div>
 
-  </div>
-  )}
+);
+}  
+
 
 export default App;
